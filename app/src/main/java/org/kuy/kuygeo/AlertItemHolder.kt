933ba33fc8acf0656e77a55fd832715ea7e3efbf
ariel.gamez <L0g Sp!ral>
2019-04-02 20:06:31 -0300

    Agrego activity principal con la lista de las alertas

---
+++ app/src/main/java/org/kuy/kuygeo/AlertItemHolder.kt
@@ -0,0 +1,7 @@
+package org.kuy.kuygeo
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
+class AlertItemHolder(val view: View) : RecyclerView.ViewHolder(view) {
+}
---
+++ app/src/main/java/org/kuy/kuygeo/MainActivity.kt
@@ -0,0 +1,36 @@
+package org.kuy.kuygeo
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        initializeComponents()
+    }
+
+    private fun initializeComponents(){
+        fab.setOnClickListener { view ->
+            goToMapActivity()
+        }
+        alertsRV.layoutManager = LinearLayoutManager(this)
+        alertsRV.adapter = AlertAdapter()
+    }
+
+    private fun goToMapActivity() {
+        val intent = Intent(this, MapsActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+}
